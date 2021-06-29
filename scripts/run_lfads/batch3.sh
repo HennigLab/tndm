7111128c030d2103f47ba2826ade46749238dda4
alessandrofacchin <alessandro.facchin@outlook.com>
2021-06-29 17:57:20 +0000

    Fixed gitignore and new scripts

---
+++ scripts/run_lfads/batch3.sh
@@ -0,0 +1,7 @@
+for BEHAVIOUR_SIGMA in "0.5" "1.0" "2.0"
+do
+    for TRIALS in "50" "100" "200"
+    do
+        python latentneural -r "latentneural/data/storage/lorenz/grid/train_trials=$TRIALS|baselinerate=15|behaviour_sigma=$BEHAVIOUR_SIGMA/training_settings.yaml"
+    done
+done
