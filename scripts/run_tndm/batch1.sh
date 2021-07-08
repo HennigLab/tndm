f6177de763df022ab3e07dc35486c2267fc192b8
alessandrofacchin <alessandro.facchin@outlook.com>
2021-07-08 21:58:28 +0000

    Updated bash scripts

---
+++ scripts/run_tndm/batch1.sh
@@ -0,0 +1,7 @@
+for BEHAVIOUR_SIGMA in "0.5" "1.0" "2.0"
+do
+    for TRIALS in "50" "100"
+    do
+        python latentneural -r "latentneural/data/storage/lorenz/grid/train_trials=$TRIALS|baselinerate=5|behaviour_sigma=$BEHAVIOUR_SIGMA/training_settings.tndm.yaml"
+    done
+done
