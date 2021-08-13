240b7c4e5fe42f2ca9dd3d6272273bd756fe89ba
alessandrofacchin <alessandro.facchin@outlook.com>
2021-08-13 07:24:50 +0000

    Removed scripts

--- scripts/run_tndm/batch2.sh
+++
@@ -1,7 +0,0 @@
-for BEHAVIOUR_SIGMA in "0.5" "1.0" "2.0"
-do
-    for TRIALS in "50" "100"
-    do
-        python latentneural -r "latentneural/data/storage/lorenz/grid/train_trials=$TRIALS|baselinerate=10|behaviour_sigma=$BEHAVIOUR_SIGMA/training_settings.tndm.yaml"
-    done
-done
