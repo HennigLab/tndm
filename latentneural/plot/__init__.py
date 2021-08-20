4403cc352eb19a79ed9865a0fceaded3210558e9
Alessandro Facchin <39587443+alessandrofacchin@users.noreply.github.com>
2021-08-20 13:49:16 +0300

    Delete latentneural/plot directory

--- latentneural/plot/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lorenz_dynamics import lorenz_dynamics
-from .lorenz_behaviour_weights import lorenz_behaviour_weights
-from .lorenz_behaviour_dynamics import lorenz_behaviour_dynamics
-from .lorenz_neural_dynamics import lorenz_neural_dynamics
